7a7a75a5cbe35fecb0f8793b13c5b1ac085d7665
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-02-18 14:42:44 -0300

    Refatorando para Styled Components

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,31 +1,63 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import styled from 'styled-components'
 
 type Props = {
   aoPesquisar: (termo: string) => void
 }
+
+const Formulario = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: var(--cor-secundaria);
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+`
+
+const Campo = styled.input.attrs({ type: 'search' })`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  height: 40px;
+`
+
+const BotaoPesquisar = styled.button`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
 
 const FormVagas = ({ aoPesquisar }: Props) => {
   const [termo, setTermo] = useState<string>('')
 
   const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    aoPesquisar(termo.toLocaleLowerCase())
+    aoPesquisar(termo.toLowerCase())
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Formulario onSubmit={aoEnviarForm}>
+      <Campo
         placeholder="Front-end, fullstack, node, design"
-        onChange={(e) => setTermo(e.target.value)}
-        type="search"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setTermo(e.target.value)
+        }
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </Formulario>
   )
 }
+
 export default FormVagas
